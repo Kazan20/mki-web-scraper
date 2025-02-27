aa969e46e868899d0dc117240791d61daf1720d9
Ariel Zvinowanda <kazan@outlook.ph>
2025-02-27 17:07:00 +0200

    Updated User Input

--- main.go
+++ main.go
@@ -1,36 +1,47 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "github.com/gocolly/colly/v2"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-    // Create a new collector
-    c := colly.NewCollector(
-        colly.AllowedDomains("example.com"), // Replace with the domain you want to scrape
-    )
+	// Get URL from user
+	fmt.Print("Enter the URL to scrape: ")
+	reader := bufio.NewReader(os.Stdin)
+	userURL, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Error reading input:", err)
+	}
+	userURL = strings.TrimSpace(userURL)
 
-    // On every a element which has href attribute call callback
-    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-        link := e.Attr("href")
-        fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-    })
+	// Create a new collector
+	c := colly.NewCollector()
 
-    // Before making a request print "Visiting ..."
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL.String())
-    })
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	})
 
-    // Print if error occurs
-    c.OnError(func(r *colly.Response, err error) {
-        log.Printf("Error while scraping: %v\n", err)
-    })
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
 
-    // Start scraping
-    err := c.Visit("http://example.com") // Replace with the URL you want to scrape
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Print if error occurs
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Error while scraping: %v\n", err)
+	})
+
+	// Start scraping
+	err = c.Visit(userURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
